ec6a3ab2439b235c4eebfc2ea4638d1ac92e7924
José Isaac <joseisaacnascimento@gmail.com>
2025-10-29 16:17:30 -0300

    feat(home server): criar página Home como server component para usar metadados (title) - importar a Home client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+// para definir o titulo da pagina
+export const metadata = {
+  title: "Nova Memória",
+};
+
+// importando a pagina
+import HomeClient from "./pageClient";
+
+// retorna a pagina
 export default function Home() {
-  return <main></main>;
+  return <HomeClient />;
 }
