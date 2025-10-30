024ef4848540497c6c8d62cee7f5067a1c75eb0b
José Isaac <joseisaacnascimento@gmail.com>
2025-10-30 17:12:03 -0300

    feat(not-found): adicionar logo na página not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Home } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <main>
-      <section className="h-screen w-full flex gap-2 flex-col items-center justify-center text-center">
+    <main className="min-h-screen w-full flex items-center justify-center bg-linear-to-br from-zinc-900 via-black to-zinc-950 text-white">
+      <section className="flex gap-2 flex-col items-center justify-center text-center">
+        {/* Logo da aplicação (opcional) */}
+        <div className="flex flex-col items-center gap-2">
+          <Image
+            src="/assets/images/logo.png"
+            alt="Logo Memória"
+            width={250}
+            height={250}
+          />
+        </div>
+
         <h1 className="text-2xl md:text-4xl font-bold">
           404 - Página não encontrada
         </h1>
@@ -15,7 +26,7 @@ export default function NotFound() {
           Desculpe, a página que voce procurou não foi encontrada.
         </p>
         <Link href="/">
-          <Button className="mt-4">
+          <Button variant="outline" className="text-black mt-4 cursor-pointer">
             Voltar para a home <Home />
           </Button>
         </Link>
