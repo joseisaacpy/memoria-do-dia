4a13a46dbd2e77c49a63580c65db5974356055c5
José Isaac <joseisaacnascimento@gmail.com>
2025-11-03 14:21:05 -0300

    refactor(login): refatorar página de login - remover icon de login

--- app/loginClient.tsx
+++ app/loginClient.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { LogIn } from "lucide-react";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 
